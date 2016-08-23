e7868c5f59caee4e706c7d7dd4956a0aa730b846
Craig Beck <luckybonza@gmail.com>
2016-08-22 22:38:55 -0700

    add service tests

---
+++ test/server.js
@@ -0,0 +1,86 @@
+const expect = require('chai').expect;
+const server = require('../lib');
+
+describe('service endpoints', () => {
+
+  describe('POST /server', () => {
+
+    it('should accept metrics', done => {
+
+      const req = {
+        method: 'POST',
+        url: '/server',
+        payload: {
+          name: 'foobar',
+          cpu: 0.13,
+          ram: 0.41
+        }
+      };
+
+      server.inject(req, response => {
+
+        expect(response.statusCode).to.equal(200);
+
+        expect(response.result)
+          .to.have.property('link')
+          .to.have.property('server', '/server/foobar');
+
+        done();
+      });
+    });
+  });
+
+  describe('GET  /server/{id}', () => {
+
+    it('should respond with not found for unknown server id', done => {
+
+      const req = {
+        method: 'GET',
+        url: '/server/foobar-123'
+      };
+
+      server.inject(req, response => {
+
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+
+    it('should respond with metrics for known server id', done => {
+
+      const post = {
+        method: 'POST',
+        url: '/server',
+        payload: {
+          name: 'foobar-137',
+          cpu: 0.12,
+          ram: 0.33
+        }
+      };
+
+      server.inject(post, response => {
+        const req = {
+          method: 'GET',
+          url: response.result.link.server
+        };
+
+        server.inject(req, response => {
+
+          expect(response.statusCode).to.equal(200);
+
+          expect(response.result)
+            .to.have.property('name', 'foobar-137');
+
+          expect(response.result)
+            .to.have.property('link')
+            .to.have.property('self', '/server/foobar-137');
+
+          expect(response.result)
+            .to.have.property('stats');
+
+          done();
+        });
+      });
+    });
+  });
+});
